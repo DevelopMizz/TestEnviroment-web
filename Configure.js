bbccae61811f60b169e64c68eca52503a507118a
Josh Mizzi <DevelopMizz@users.noreply.github.com>
2020-05-11 19:58:43 +0100

    Update Configure.js

--- Configure.js
+++ Configure.js
@@ -1,4 +1,8 @@
 !(g.mizLib.testEnviroment) || g.mizLib.testEnviroment.config = new function(){
+  this.enviromentConfig = new function(){
+    this.testSet = g.mizLib.testEnviroment.defaultTestSet
+  }
+  
   this.testConfig = new Function(){
     this.enableDependanceyFailureTesting = false;
     this.DependanceyFailureRate = 0;                           // 0 out of 10.
