c04822c31664fb8e606e55b62bf0ec39973ad082
Josh Mizzi <DevelopMizz@users.noreply.github.com>
2020-05-11 19:39:44 +0100

    Update Configure.js

--- Configure.js
+++ Configure.js
@@ -1,14 +1,13 @@
-GlobalObject= Window || Global;
-
-GloabalObject.config = {
-  testConfig = new Function(){
-    enableDependanceyFailureTesting = false;
-    DependanceyFailureRate = 0;                           // 0 out of 10.
-    testType = null;                                      // if multiple use arraay.
-    condt DetectTedtType = 0;
+!(g.MizLib.TestEnviroment) || g.MizLib.TestEnviroment.config = new function(){
+  this.testConfig = new Function(){
+    this.enableDependanceyFailureTesting = false;
+    this.DependanceyFailureRate = 0;                           // 0 out of 10.
+    this.testType = null;                                      // if multiple use arraay.
+    const DetectTedtType = 0;
     const SudoRandom = 1;
     const Systematic = 2;
-    const Unit = 3
+    const Unit = 3;
     const memreyLeak = 4;
   }
 }
+
